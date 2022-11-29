5a1ba173896319c2d6713c53fe047603e393803a
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-11-29 19:14:25 +0800

    create prelude identifiers file

---
+++ src/main/resources/prelude.pas
@@ -0,0 +1,25 @@
+{
+IntelliPascal - Free Pascal for IntelliJ Platform
+Copyright (C) 2022 HTGAzureX1212.
+
+IntelliPascal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+IntelliPascal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with IntelliPascal. If not, see <https://www.gnu.org/licenses/>.
+}
+
+{
+The Pascal Prelude Identifiers unit.
+
+This unit provides predefined identifiers for code completion of these identifiers.
+}
+
+unit $prelude;
