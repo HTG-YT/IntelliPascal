254940e633da0d8c4c900b8111240bcfcbdab2af
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-11-30 18:50:01 +0800

    add pascalsdk module

---
+++ src/main/kotlin/com/github/htgazurex1212/intellipascal/pascalSdk/FreePascalSdkType.kt
@@ -0,0 +1,10 @@
+package com.github.htgazurex1212.intellipascal.pascalSdk
+
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.projectRoots.SdkType
+
+class FreePascalSdkType : SdkType("FreePascal SDK") {
+  companion object {
+    private val LOGGER = Logger.getInstance(FreePascalSdkType::class.java.name)
+  }
+}
